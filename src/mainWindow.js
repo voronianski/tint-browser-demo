8d9cf3b33082fd52fd6325d1a58a92c546587af5
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-12-16 00:50:16 +0200

    add file

---
+++ src/mainWindow.js
@@ -0,0 +1,27 @@
+var Screens = require('Screens');
+var Window = require('Window');
+
+var mainMenu = require('./menu');
+
+MainWindow = exports;
+
+MainWindow.init = function () {
+    // Create main application window
+    var win = MainWindow.sharedInstance = new Window();
+
+    win.canBeFullscreen = true;
+
+    // Access actual screen size for window
+    win.width = Screens.active.bounds.width - 300;
+    win.height = Screens.active.bounds.height;
+    win.x = Screens.active.bounds.x + 300;
+    win.y = Screens.active.bounds.y;
+
+    win.title = application.name;
+
+    // Create application menu
+    win.menu = mainMenu.create();
+
+    // Show window when all manipulations are done
+    win.visible = true;
+};
